9f4a78544d98a1693c1b5795998c0519918371d2
imtiazsamir1 <imtiazsamir416@gmail.com>
2025-11-01 17:11:52 +0600

    first commit

---
+++ set.js
@@ -0,0 +1,5 @@
+console.time("task");
+for (let i = 0; i <= 600000; i++) {
+  console.log(i);
+}
+console.timeEnd("task");
---
+++ set1.js
@@ -0,0 +1,23 @@
+//const arr = ["apple", "mango", "banana", "apple"];
+
+//const set = new Set(arr);
+
+//const test = Array.from(set);
+//test.push("tomato");
+//console.log(test);
+
+//
+
+//const arr = ["apple", "mango", "banana", "apple"];
+
+//const set = new Set(arr);
+
+//console.log(set.has("tomato"));
+
+//
+const arr = ["apple", "mango", "banana", "apple"];
+
+const set = new Set(arr);
+
+console.log(set.delete("apple"));
+console.log(set);
