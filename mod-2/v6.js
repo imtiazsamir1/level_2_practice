d0ee411634a844e5c8d8850e56e0fea5f11f4987
imtiazsamir1 <imtiazsamir416@gmail.com>
2025-11-03 15:15:24 +0600

    normalization

---
+++ mod-2/v6.js
@@ -0,0 +1,14 @@
+const postsArray = [
+  { id: "p-101", title: "Intro to SQL", author: "Alex" },
+  { id: "p-102", title: "Data Structures in JS", author: "Beth" },
+  { id: "p-103", title: "Understanding Reduce", author: "Chris" },
+  { id: "p-104", title: "CSS Grid Tricks", author: "Alex" },
+];
+
+const lockup = postsArray.reduce((table, post) => {
+  table[post.id] = post;
+  return table;
+}, {});
+console.log(lockup);
+const post = lockup["p-101"];
+console.log(post);
