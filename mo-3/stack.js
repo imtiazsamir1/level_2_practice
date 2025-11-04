1b35674526aa3e45d93713704bc10a1d668f2ec6
imtiazsamir1 <imtiazsamir416@gmail.com>
2025-11-04 23:31:14 +0600

    stack

---
+++ mo-3/stack.js
@@ -0,0 +1,81 @@
+// class Stack {
+//   constructor() {
+//     this.items = [];
+//   }
+//   push(value) {
+//     this.items.push(value);
+//   }
+
+//   pop() {
+//     if (this.isEmpty()) {
+//       return undefined;
+//     }
+//     return this.items.pop();
+//   }
+
+//   peek() {
+//     if (this.isEmpty()) {
+//       return undefined;
+//     }
+//     return this.items[this.items.length - 1];
+//   }
+//   isEmpty() {
+//     return this.items.length === 0;
+//   }
+//   print() {
+//     console.log(this.items);
+//   }
+// }
+// const stack = new Stack();
+
+// stack.push(10);
+// stack.push(20);
+// stack.push(40);
+// stack.push(500);
+// stack.print;
+// //console.log(stack.peek());
+// console.log(stack.pop());
+// console.log(stack.pop());
+// console.log(stack.peek());
+
+class Stack {
+  constructor() {
+    this.items = [];
+  }
+  push(value) {
+    this.items.push(value);
+  }
+  pop() {
+    if (this.isEmpty()) {
+      return undefined;
+    }
+    return this.items.pop();
+  }
+  peek() {
+    if (this.isEmpty()) {
+      return undefined;
+    }
+    return this.items[this.items.length - 1];
+  }
+  isEmpty() {
+    return this.items.length === 0;
+  }
+  print() {
+    console.log(this.items.slice().reverse().join(" -> "));
+  }
+}
+const stack = new Stack();
+
+stack.push(30);
+stack.push(40);
+stack.push(50);
+stack.push(60);
+stack.push(70);
+stack.print();
+console.log(stack.peek()); //peek
+console.log(stack.pop());
+console.log(stack.pop());
+console.log(stack.pop());
+console.log(stack.pop());
+console.log(stack.pop());
+console.log(stack.pop());
