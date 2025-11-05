7e9ab1fabb57985bcfc5f86b92cf0c335606bdd6
imtiazsamir1 <imtiazsamir416@gmail.com>
2025-11-05 22:32:14 +0600

    remove

---
+++ mod-2/v7.js
@@ -0,0 +1,25 @@
+const surveyResponses = [
+  "A",
+  "C",
+  "B",
+  "A",
+  "B",
+  "B",
+  "C",
+  "A",
+  "B",
+  "D",
+  "A",
+  "C",
+  "B",
+  "A",
+];
+const count = surveyResponses.reduce((table, response) => {
+  if (table[response]) {
+    table[response] = table[response] + 1;
+  } else {
+    table[response] = 1;
+  }
+  return table;
+}, {});
+console.log(count);
---
+++ mod-2/v9.js
@@ -0,0 +1,13 @@
+const users = [
+  { id: 101, name: "Alice" },
+  { id: 102, name: "Bob" },
+  { id: 103, name: "Charlie" },
+];
+
+const posts = [
+  { id: 1, userId: 102, title: "My first post" },
+  { id: 2, userId: 101, title: "React Hooks" },
+  { id: 3, userId: 101, title: "Data Structures" },
+  { id: 4, userId: 103, title: "CSS is fun" },
+  { id: 5, userId: 102, title: "Node.js streams" },
+];
